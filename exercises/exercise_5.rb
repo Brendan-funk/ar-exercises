2898e195584d1ccad1d7343652e9dae5d7982833
Brendan-funk <brendanzfunk@gmail.com>
2022-04-04 20:54:06 +0000

    finishes exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Sum"
+puts Store.sum("annual_revenue")
+puts "Average"
+puts Store.average("annual_revenue")
+puts "Over 1,000,000"
+puts Store.where("annual_revenue > ?", 1000000).count
